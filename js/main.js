65746ae944dbec476c09715aa0d6e3c3aa1c60f5
Marsel <chelovek-gorod@yandex.by>
2021-10-20 13:32:20 +0300

    edit task

--- js/main.js
+++ js/main.js
@@ -1,35 +1,54 @@
 "use strict";
 
-const titles = document.getElementById('titles');
-const categorys = document.getElementById('categorys');
+const categories = document.getElementById('categories');
+const dataList = document.getElementById('dataList');
 const result = document.getElementById('result');
 
+let categoriesReady = false;
 let dataStorage;
 
-const url = 'https://api.publicapis.org/'
-const paramCategory = 'entries?category=animals&https=true'
-
-fetch(url + paramCategory)
-    .then(response => response.json())
-    .then(json => getData(json.entries))
-    .catch(err => console.log('Fetch problem: ' + err.message));
-
-function getData(data) {
-    dataStorage = data;
-    data.forEach(e => titles.innerHTML += `<option value="${e.API}">${e.API}</option>`);
+const url = 'https://api.publicapis.org/';
+const paramCategories = 'categories';
+function getFetchParam(category) {
+    return `entries?category=${category}&https=true`;
 }
 
-function changeTitles() {
-    let object = dataStorage.find(e => e.API === titles.value)
-    categorys.innerHTML = '<option selected value="onload">unset</option>';
-    for (let keys in object) categorys.innerHTML += `<option value="${keys}">${keys}</option>`;
-    categorys.selected = 'unset';
+getData();
+
+function getData(param = paramCategories) {
+    fetch(url + param)
+    .then(response => response.json())
+    .then(data => {
+        if (param === paramCategories) {
+            data.forEach(e => categories.innerHTML += `<option value="${e}">${e}</option>`);
+            categoriesReady = true;
+        } else {
+            setData(data.entries);
+        }
+    })
+    .catch(err => result.innerHTML = `<span class="error">Fetch problem: ${err.message}</span>`);
+}
+
+categories.onchange = function () {
+    if (categoriesReady) {
+        let param = getFetchParam(categories.value);
+        getData(param);
+    }
+}
+
+function setData(data) {
+    dataStorage = data;
+    for (let key in data) dataList.innerHTML += `<option value="${key}">${data[key].API}</option>`;
+    dataList.selected = 'unset';
     result.innerHTML = 'unset';
 }
 
-function changeCategorys() {
-    let object = dataStorage.find(e => e.API === titles.value);
-    result.innerHTML = (categorys.value == 'Link') ?
-        `<a href="${object[categorys.value]}" target="_blank">${object[categorys.value]}</a>` :
-        object[categorys.value];
+function changeDataList() {
+    let object = dataStorage[dataList.value];
+    result.innerHTML = '';
+    result.innerHTML += object.Auth ? `<span>Auth :</span> ${object.Auth} <br>` : '';
+    result.innerHTML += object.Description ? `<span>Description :</span> ${object.Description} <br>` : '';
+    result.innerHTML += object.Cors ? `<span>Cors :</span> ${object.Cors} <br>` : '';
+    result.innerHTML += object.HTTPS ? `<span>HTTPS :</span> ${object.HTTPS} <br>` : '';
+    result.innerHTML += object.Link ? `<a href="${object.Link}" target="_blank">${object.Link}</a>` : '';
 }
